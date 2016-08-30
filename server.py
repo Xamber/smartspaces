7058c9b05df02984ee85fcbea4e36918bbc158cd
Artem <xamber-zenit@mail.ru>
2016-08-30 09:31:42 +0300

    Добавлен фронтенд

--- server.py
+++ server.py
@@ -1,15 +1,19 @@
+import os
+
+import jinja2
 from aiohttp import web
 import asyncio
+import aiohttp_jinja2
 
 
+
+@aiohttp_jinja2.template('index.html')
 class IndexHandler(web.View):
-
     async def get(self):
-        return web.Response(body=b"Smart Spaces")
+        return {}
 
 
 class WebSocketHandler(web.View):
-
     async def get(self):
         return web.Response(body=b"Smart Spaces")
 
@@ -24,10 +28,16 @@ async def create_app():
 
     app.router.add_static('/static', 'static')
 
+    template_dir = os.path.dirname(os.path.abspath(__file__)) + '/templates/'
+    aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader(template_dir), context_processors=[])
+
     return app
 
 
 if __name__ == '__main__':
+
+
+
     loop = asyncio.get_event_loop()
     app = loop.run_until_complete(create_app())
     web.run_app(app, port=1337)
